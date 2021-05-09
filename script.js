bbbd63984968c280f2bf0d0e4d1e0a7bda00fba1
Aman Prakash Jha <amanjha7151@gmail.com>
2021-05-10 03:33:16 +0530

    ui

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //Selectors
-
+const h = document.getElementById("hours").value
+const m = document.getElementById("minutes").value
+const s = document.getElementById("seconds").value
 
 
 //Event Listeners
